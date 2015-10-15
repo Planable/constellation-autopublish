13a014102dac0e96aaebbd1cb2ab6842c40daf53
Brent Abrahams <brent_abrahams@yahoo.com>
2015-10-15 22:27:00 +0800

    Updated dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'constellation:autopublish',
-  version: '0.3.5',
+  version: '0.3.6',
   summary: 'Autopublish toggle plugin for Constellation',
   git: 'https://github.com/JackAdams/constellation-autopublish.git',
   documentation: 'README.md',
@@ -12,7 +12,7 @@ Package.onUse(function(api) {
 
   api.use(['templating','tracker'], 'client');
   api.use('underscore');
-  api.use('constellation:console@1.2.2', 'client');
+  api.use('constellation:console@1.2.3', 'client');
   api.use('dburles:mongo-collection-instances@0.3.4', 'server');
 
   api.addFiles('autopublish.html','client');
