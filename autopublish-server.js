9115b2a1285ba3caf51965f5a046170b22b4fdf4
Brent Abrahams <brent_abrahams@yahoo.com>
2015-12-09 22:57:11 +0800

    Updated dependencies

--- autopublish-server.js
+++ autopublish-server.js
@@ -2,7 +2,7 @@ Meteor.publish('Constellation_autopublish', function (collectionNames) {
   check(collectionNames, [String]);
   return _.reduce(collectionNames, function (memo, collectionName) {
     var collection = Package["constellation:console"].Constellation.Collection(collectionName);
-    if (collection) {
+    if (collectionName && collection) {
       memo.push(collection.find());
     }
     return memo;
